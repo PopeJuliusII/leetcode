8b94e044753251c152763809ff7791395b7c8cfe
PopeJuliusII <edgarb@tuta.io>
2021-12-21 10:51:57 +0100

    Added solution 2 to solutions.py in problem 27.

--- Problem 27 - Remove Element/solutions.py
+++ Problem 27 - Remove Element/solutions.py
@@ -7,3 +7,12 @@ def solution_1(nums: list[int], val: int) -> list[int]:
         else:
             left_swap += 1
     return right_swap + 1
+
+
+def solution_2(nums: list[int], val: int) -> list[int]:
+    idx = 0
+    for n in nums:
+        if n != val:
+            nums[idx] = n
+            idx += 1
+    return idx
