d9dd9fd283e05c252f4e8bb2860550391181d5d3
PopeJuliusII <edgarb@tuta.io>
2022-01-14 14:07:10 +0100

    Added solution 2 to solutions.py in problem 83.

--- Problem 83 - Remove Duplicates from Sorted List/solutions.py
+++ Problem 83 - Remove Duplicates from Sorted List/solutions.py
@@ -2,7 +2,7 @@ from list_node import ListNode
 from typing import Optional
 
 
-def solution_1(head: Optional[ListNode] = None) -> list[int]:
+def solution_1(head: Optional[ListNode] = None) -> Optional[ListNode]:
     ret_node = head
     while head and head.next:
         if head.val == head.next.val:
@@ -10,3 +10,12 @@ def solution_1(head: Optional[ListNode] = None) -> list[int]:
         else:
             head = head.next
     return ret_node
+
+
+def solution_2(head: Optional[ListNode] = None, parent: Optional[int] = None) -> Optional[ListNode]:
+    if head is None:
+        return head
+    if head.val == parent:
+        return solution_2(head.next, head.val)
+    head.next = solution_2(head.next, head.val)
+    return head
